13ba2bf006db107d1575d356f32c9b5fe986ca69
RocoElWuero <usuario917@gmail.com>
2023-01-18 11:59:04 -0600

    tui-image-editor2

---
+++ MarkDocMVC/wwwroot/js/tui-image-editor.js
@@ -0,0 +1,10 @@
+﻿const ImageEditor = import('../node_modules/tui-image-editor/dist/tui-image-editor.js');
+//const ImageEditor = require('../node_modules/tui-image-editor/dist/tui-image-editor');
+const instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+    cssMaxWidth: 700,
+    cssMaxHeight: 500,
+    selectionStyle: {
+        cornerSize: 20,
+        rotatingPointOffset: 70,
+    },
+});
